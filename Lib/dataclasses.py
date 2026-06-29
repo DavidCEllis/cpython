19c0a5807ccfbc19d0c4089bf4a0fe64ea072469
David C Ellis <ducksual@gmail.com>
2026-06-29 16:04:39 +0100

    Use .get and check if falsy instead of checking for presence in `sys.modules` before retrieving.

--- Lib/dataclasses.py
+++ Lib/dataclasses.py
@@ -879,9 +879,9 @@ class _AutoMethod:
 def _source_to_method(cls, name, source, locals=None, annotate=None, decorator=None):
     # This takes generated source code and local names and converts it into
     # a real method. Needed for dataclass methods generated from source templates.
-    if cls.__module__ in sys.modules:
+    if module := sys.modules.get(cls.__module__):
         # Make a copy to avoid mutating module globals if `__builtins__` doesn't exist
-        globals = sys.modules[cls.__module__].__dict__.copy()
+        globals = module.__dict__.copy()
     else:
         # Theoretically this can happen if someone writes
         # a custom string to cls.__module__.  In which case
